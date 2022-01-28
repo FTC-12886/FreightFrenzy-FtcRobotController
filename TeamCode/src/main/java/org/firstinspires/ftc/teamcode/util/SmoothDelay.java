09d9109d39ff64ee477cdd8424af229e0b357be0
ToothbrushB <49379586+ToothbrushB@users.noreply.github.com>
2022-01-28 07:42:04 -0600

    Combine SmoothDelay and SmoothDelayimpl

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/SmoothDelay.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/SmoothDelay.java
@@ -1,10 +1,84 @@
 /*
- * Jay Jasper smooth_delay.h
+ * Jay Jasper smooth_delay.c
  * Ported to Java by Caleb Li
  */
 package org.firstinspires.ftc.teamcode.util;
 
-public abstract class SmoothDelay {
+public class SmoothDelay {
+    private final SmoothDelayInfo profile;
+
+    public SmoothDelay(int steps) {
+        this.profile = profileSmoothDelaySetup(steps);
+    }
+
+    private SmoothDelayInfo profileSmoothDelaySetup(int steps) {
+        SmoothDelayInfo profile = new SmoothDelayInfo();
+        profile.stepCurrent = 0;
+        profile.steps = steps;
+
+        // init coefficients
+        profile.coefficients = new double[steps];
+        double sum = 0;
+
+        // fill coefficients
+        for (int i = 0; i < profile.steps; ++i) {
+            double t = i;
+            profile.coefficients[i] = -(t + 1) * (t - profile.steps);
+            sum += profile.coefficients[i];
+        }
+
+        // normalize coefficients
+        for (int i = 0; i < profile.steps; ++i) {
+            profile.coefficients[i] /= sum;
+        }
+
+        // initialize the input history array
+        profile.history = new double[steps];
+        for (int i = 0; i < profile.steps; ++i) {
+            profile.history[i] = 0;
+        }
+
+        return profile;
+    }
+
+    // updates history with the new input, spits out a filtered value
+    // CAUTION - relies on calling loop to operate at a constant frequency (can we do that??)
+
+    public double profileSmoothDelaySmooth(double curentRaw) {
+        profile.history[profile.stepCurrent] = curentRaw;
+
+        // discrete convolution integral;
+        double result = 0;
+        for (int historyIndex = 0; historyIndex < profile.steps; ++historyIndex) {
+            int profileIndex = historyIndex - profile.stepCurrent - 1;
+
+            if (profileIndex >= profile.steps) {
+                profileIndex -= profile.steps;
+            }
+            if (profileIndex < 0) {
+                profileIndex += profile.steps;
+            }
+
+            // actually compute the integral
+            result += profile.coefficients[profileIndex] * profile.history[historyIndex];
+        }
+        profile.stepCurrent = (profile.stepCurrent + 1) % profile.steps;
+        return result;
+    }
+
+
+    public void profileSmoothDelayFree() {
+        // no need to free anything, Java handles memory allocation on its own
+    }
+
+
+    public void profileSmoothDelayPrintCoefficients() {
+        System.out.printf("Profile Coefficients: %d\n", profile.steps);
+        for (int i = 0; i < profile.steps; ++i) {
+            System.out.printf("%s\t", profile.coefficients[i]);
+        }
+        System.out.print("\n");
+    }
     public static class SmoothDelayInfo{
 
         public int steps;
@@ -12,13 +86,4 @@ public abstract class SmoothDelay {
         public double[] coefficients;
         public double[] history;
     }
-
-//    private abstract SmoothDelayInfo profileSmoothDelaySetup(int steps);
-
-    public abstract double profileSmoothDelaySmooth(double curentRaw);
-
-    public abstract void profileSmoothDelayFree();
-
-    public abstract void profileSmoothDelayPrintCoefficients();
-
 }
